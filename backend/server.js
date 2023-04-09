6386879dfedc55cdaacf4c63a6a9325f5f47f8a7
Sanjay Mansel <mansel@Sanjays-MacBook-Air.local>
2023-04-09 16:57:30 +0900

    Updating server

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "prod") {
   console.log(__dirname);
 
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
