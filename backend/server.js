343102d3535c0239c4d8d97cd4dd0853f8a61a0f
Sanjay Mansel <mansel@Sanjays-MacBook-Air.local>
2023-05-26 19:02:29 +0900

    Correcting prod build

--- backend/server.js
+++ backend/server.js
@@ -15,32 +15,32 @@ const app = express();
 
 app.use(express.json());
 
-dbConn.query(
-  "SELECT group_id, group_name, start_date, budget from u932593839_ipl.groups where group_id=9",
-  function (err, rows, fields) {
-    console.log("value of err: ", err);
-    console.log("value of rows: ", rows);
-    // console.log("value of fields: ", fields);
-  }
-);
+// dbConn.query(
+//   "SELECT group_id, group_name, start_date, budget from u932593839_ipl.groups where group_id=9",
+//   function (err, rows, fields) {
+//     console.log("value of err: ", err);
+//     console.log("value of rows: ", rows);
+//     // console.log("value of fields: ", fields);
+//   }
+// );
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/expense", expenseRoutes);
 
-// const __dirname = path.resolve();
-// if (process.env.NODE_ENV === "prod") {
-//   console.log(__dirname);
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "prod") {
+  console.log(__dirname);
 
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.sendFile("Server is running!! YAH!!!!!!!!");
-//   });
-// }
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.sendFile("Server is running!! YAH!!!!!!!!");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
